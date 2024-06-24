40622e9fdfd3e4e09dfdc87a054f2cf389e48a63
Admin <dattle329@gmail.com>
2024-06-24 19:11:03 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <header>
+        <a href="#"><img src="src\assets\img\Dmission.png" alt="" /></a>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Our Service</a></li>
+          <li><a href="">Case Study</a></li>
+          <li><a href="">Career</a></li>
+        </ul>
+        <div className='toggle'>
+          <i class="bi bi-list"></i>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Our Service</a></li>
+            <li><a href="">Case Study</a></li>
+            <li><a href="">Career</a></li>
+          </ul>
+        </div>
+      </header>
+      <body>
+        <h1>News & Blogs</h1>
+        <div className="item">
+          <div className="blog-item">
+            <a href=""><img src="src\assets\img\billboad.png" alt="" /></a>
+            <div className="blog-item-content">
+              <span>27 May 2024</span><br />
+              <span>Navigating the Future: Top 10 Block chain Trends in 2024</span>
+            </div>
+          </div>
+          <div className="blog-item">
+            <a href=""><img src="src\assets\img\billboad.png" alt="" /></a>
+            <div className="blog-item-content">
+              <span>27 May 2024</span><br />
+              <span>Navigating the Future: Top 10 Block chain Trends in 2024</span>
+            </div>
+          </div>
+          <div className="blog-item">
+            <a href=""><img src="src\assets\img\billboad.png" alt="" /></a>
+            <div className="blog-item-content">
+              <span>27 May 2024</span><br />
+              <span>Navigating the Future: Top 10 Block chain Trends in 2024</span>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  )
+}
+
+export default App
